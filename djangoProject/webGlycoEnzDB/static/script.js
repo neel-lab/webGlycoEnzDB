29456bc9f6ade9e61c8ac0dae604ff83511858f9
Vishnu Ghosh <vishnugh@gmail.com>
2023-04-13 16:29:05 -0400

    Ontology UI changes

--- djangoProject/webGlycoEnzDB/static/script.js
+++ djangoProject/webGlycoEnzDB/static/script.js
@@ -13,6 +13,9 @@ function showPathways() {
 }
 
 document.addEventListener("DOMContentLoaded", function () {
+    const RIGHT_ARROW = '▶';
+    const DOWN_ARROW = '▼';
+
     showFunctions();
     document.querySelectorAll('.sidebar .nav-link').forEach(function (element) {
 
@@ -25,9 +28,25 @@ document.addEventListener("DOMContentLoaded", function () {
             //  Clear all syblings and sub links styles            
             if (parentEl && parentEl.parentElement){
             const childNodes = parentEl.parentElement.querySelectorAll('a');
+
+            const symbol_span = element.querySelector('span');
+            if (symbol_span && symbol_span.innerHTML === DOWN_ARROW){
+                symbol_span.innerHTML = RIGHT_ARROW;
+            }
+            else if (symbol_span && symbol_span.innerHTML === RIGHT_ARROW){
+                symbol_span.innerHTML = DOWN_ARROW;
+            }
+
             childNodes.forEach(childNode => {
                 childNode.classList.remove('fw-bold');
                 childNode.classList.remove('text-primary');
+
+                if (element !== childNode){
+                  const symbol_span =  childNode.querySelector('span');
+                    if (symbol_span && symbol_span.innerHTML === DOWN_ARROW){
+                        symbol_span.innerHTML = RIGHT_ARROW;
+                    }
+                }
             });
 
         }
